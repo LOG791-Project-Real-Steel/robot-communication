1e1174404a266e47cdbd71246810fec187bf6d5a
Adam Mihajlovic <mihajlovicadam@gmail.com>
2025-08-02 14:27:04 -0400

    Added logs

--- SimpleTunnel/WebSocketServer/Program.cs
+++ SimpleTunnel/WebSocketServer/Program.cs
@@ -10,6 +10,19 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddSingleton<RobotWebSocketHandler>();
 
+// Clear logs
+builder.Logging.ClearProviders();
+
+// Re-add it with a timestamped formatter
+builder.Logging.AddSimpleConsole(opts =>
+{
+    // e.g. 2025-08-02 14:35:07.123 –
+    opts.TimestampFormat = "yyyy-MM-dd HH:mm:ss.fff ";
+    // Use local time; switch to true for UTC
+    opts.UseUtcTimestamp = false;
+});
+
+
 var app = builder.Build();
 
 // Configure the WebSocket request pipeline.
